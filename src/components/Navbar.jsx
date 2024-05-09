ece0d4846c84c442c93fd9ba8c88ef3356effbe1
Naimur00 <aminaim4u@gmail.com>
2024-05-10 00:37:40 +0600

    Navbar updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,61 +1,74 @@
+import { useContext } from "react";
+import { SiManjaro } from "react-icons/si";
+import { AuthContext } from "../provider/AuthProvider";
+
 const Navbar = () => {
+  const { user } = useContext(AuthContext);
   return (
-    <div className='navbar bg-base-100 shadow-sm container px-4 mx-auto'>
-      <div className='flex-1'>
-        <div className='flex gap-2 items-center'>
-          <img className='w-auto h-7' src='' alt='' />
-          <span className='font-bold'>SoloSphere</span>
-        </div>
-      </div>
-      <div className='flex-none'>
-        <ul className='menu menu-horizontal px-1'>
-          <li>
-            <div>Home</div>
-          </li>
-
-          <li>
-            <div>Login</div>
-          </li>
-        </ul>
-
-        <div className='dropdown dropdown-end z-50'>
-          <div
-            tabIndex={0}
-            role='button'
-            className='btn btn-ghost btn-circle avatar'
-          >
-            <div className='w-10 rounded-full' title=''>
-              <img
-                referrerPolicy='no-referrer'
-                alt='User Profile Photo'
-                src=''
-              />
-            </div>
+    <div className="bg-black/90">
+      <div className="navbar shadow-sm container px-4 lg:px-20 mx-auto text-white">
+        <div className="flex-1">
+          <div className="flex gap-2 items-center text-white text-2xl">
+            <SiManjaro />
+            <span className="font-bold font-briem">TomoMoni</span>
           </div>
-          <ul
-            tabIndex={0}
-            className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
-          >
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
             <li>
-              <div className='justify-between'>Add Job</div>
+              <div>Home</div>
             </li>
-            <li>
-              <div>My Posted Jobs</div>
-            </li>
-            <li>
-              <div>My Bids</div>
-            </li>
-            <li>
-              <div>Bid Requests</div>
-            </li>
-            <li className='mt-2'>
-              <button className='bg-gray-200 block text-center'>Logout</button>
-            </li>
+
+            {!user && (
+              <li>
+                <div>Login</div>
+              </li>
+            )}
           </ul>
+
+          {user && (
+            <div className="dropdown dropdown-end z-50">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full" title="">
+                  <img
+                    referrerPolicy="no-referrer"
+                    alt="User Profile Photo"
+                    src=""
+                  />
+                </div>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content text-black mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              >
+                <li>
+                  <div className="justify-between">Add Job</div>
+                </li>
+                <li>
+                  <div>My Posted Jobs</div>
+                </li>
+                <li>
+                  <div>My Bids</div>
+                </li>
+                <li>
+                  <div>Bid Requests</div>
+                </li>
+                <li className="mt-2">
+                  <button className="bg-gray-200 block text-center">
+                    Logout
+                  </button>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,10 +1,26 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../layout/Home";
+import Login from "../pages/Login";
+import Registration from "../pages/Registration";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home></Home>,
+    children: [
+      {
+        index: true,
+        element: <Home></Home>
+      },
+      {
+        path: "/login",
+        element: <Login></Login>
+      },
+      {
+        path: "/register",
+        element: <Registration></Registration>
+      }
+    ]
   },
 ]);
 
