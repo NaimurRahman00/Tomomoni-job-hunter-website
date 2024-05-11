b9bd7162d0eda20625b6f72dd2b1d557bd21d3fd
Naimur00 <aminaim4u@gmail.com>
2024-05-12 01:27:50 +0600

    my jobs Data delete method added and more

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,63 +1,76 @@
 import axios from "axios";
-import { useContext, useEffect, useState} from "react";
+import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
+import { MdDelete } from "react-icons/md";
+import toast from "react-hot-toast";
 
 const MyPostedJobs = () => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   // getting data using axios
   const [jobs, setJobs] = useState([]);
 
+  const getData = async () => {
+    const { data } = await axios(
+      `${import.meta.env.VITE_API_URL}/${user?.email}`
+    );
+    setJobs(data);
+  };
+
   useEffect(() => {
-    const getData = async () => {
-      const { data } = await axios(`${import.meta.env.VITE_API_URL}/${user?.email}`);
-      setJobs(data);
-    }
     getData();
   }, [user]);
 
-  // console.log(user.email)
-  console.log(jobs)
-
+  // Delete a data
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${import.meta.env.VITE_API_URL}/jobs/${id}`);
+      toast.success("Delete successful!");
+      getData();
+    } catch (err) {
+      console.log(err.message);
+      toast.error(err.message);
+    }
+  };
 
   return (
-    <section className="container p-20 mx-auto">
-      <div className="flex items-center gap-x-3">
+    <section className="container p-20 pt-24 mx-auto">
+      <div className="flex items-center gap-x-3 w-5/6 mx-auto">
         <h2 className="text-2xl font-medium text-white/90 font-lato">
           My Posted Jobs
         </h2>
 
-        <span className="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full ">
-          05 Jobs
+        <span className="px-3 py-1 text-sm text-black font-bold bg-blue-100 rounded-full ">
+          {jobs.length} Jobs
         </span>
       </div>
 
       <div className="flex flex-col mt-6">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden border border-gray-500 md:rounded-lg bg-zinc-800">
+            <div className="overflow-hidden w-5/6 mx-auto border border-gray-500 md:rounded-lg bg-zinc-800">
               <table className="min-w-full divide-y divide-gray-200/50 bg-zinc-800">
                 <thead className="bg-black/50">
                   <tr>
                     <th
                       scope="col"
-                      className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="py-3.5 px-4 text-sm font-medium text-left rtl:text-right text-white/80"
                     >
                       <div className="flex items-center gap-x-3">
-                        <span>Title</span>
+                        <span>Job Title</span>
                       </div>
                     </th>
 
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-white/80"
                     >
                       <span>Deadline</span>
                     </th>
 
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-white/80"
                     >
                       <button className="flex items-center gap-x-2">
                         <span>Price Range</span>
@@ -66,89 +79,79 @@ const MyPostedJobs = () => {
 
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-white/80"
                     >
                       Category
                     </th>
                     <th
                       scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
+                      className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-white/80"
                     >
-                      Description
+                      Job Applicant
                     </th>
 
-                    <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
-                      Edit
+                    <th className="px-4 py-3.5 text-sm font-medium text-left rtl:text-right text-white/80">
+                      Update/Delete
                     </th>
                   </tr>
                 </thead>
-                <tbody className="bg-zinc-800 divide-y divide-gray-200/20 ">
-                  <tr>
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      Build Dynamic Website
-                    </td>
+                <tbody className="bg-zinc-800 divide-y divide-gray-200/10 ">
+                  {jobs.map((job) => (
+                    <tr key={job._id}>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                        {job.title}
+                      </td>
 
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      10/04/2024
-                    </td>
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                        {new Date(job.applyDeadline).toLocaleDateString()}
+                      </td>
 
-                    <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                      $100-$200
-                    </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex items-center gap-x-2">
-                        <p
-                          className="px-3 py-1 rounded-full text-black bg-blue-100/60
-                           text-xs"
-                        >
-                          Web Development
-                        </p>
-                      </div>
-                    </td>
-                    <td
-                      title=""
-                      className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap"
-                    >
-                      Lorem ipsum, dolor si adipisicing elit. Ex, provident?..
-                    </td>
-                    <td className="px-4 py-4 text-sm whitespace-nowrap">
-                      <div className="flex items-center gap-x-6">
-                        <button className="text-gray-500 transition-colors duration-200   hover:text-red-500 focus:outline-none">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
+                      <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
+                        ${job.min_price} - ${job.max_price}
+                      </td>
+                      <td className="px-4 py-4 text-sm whitespace-nowrap">
+                        <div className="flex items-center gap-x-2">
+                          <p
+                            className={`px-3 py-1 rounded-full text-black
+                             text-xs font-bold ${
+                              job.category === 'On Site' && 
+                              'bg-black text-white/70'
+                             } ${ job.category === 'Remote' && 
+                             'bg-white/30 text-black/70'
+                             } ${ job.category === 'Part Time' && 
+                             'bg-white/60 text-black/70'
+                             } ${ job.category === 'Hybrid' && 
+                             'bg-black/30 text-white/70'
+                             }`}
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                            />
-                          </svg>
-                        </button>
+                            {job.category}
+                          </p>
+                        </div>
+                      </td>
+                      <td
+                        title=""
+                        className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap"
+                      >
+                        {job.job_applicant_number}
+                      </td>
+                      <td className="px-4 py-4 text-sm whitespace-nowrap">
+                        <div className="flex items-center gap-x-6">
+                          <button className="text-gray-300 transition-colors duration-200   hover:text-black hover:bg-white focus:outline-none border border-white/20 px-2 py-1 rounded-lg">
+                            Update
+                          </button>
 
-                        <button className="text-gray-500 transition-colors duration-200   hover:text-yellow-500 focus:outline-none">
-                          <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth="1.5"
-                            stroke="currentColor"
-                            className="w-5 h-5"
+                          <button
+                            onClick={() => {
+                              handleDelete(job._id);
+                            }}
+                            className="text-gray-500 transition-colors duration-200   hover:text-white focus:outline-none text-2xl"
                           >
-                            <path
-                              strokeLinecap="round"
-                              strokeLinejoin="round"
-                              d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-                            />
-                          </svg>
-                        </button>
-                      </div>
-                    </td>
-                  </tr>
+                            <MdDelete />
+                          </button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
