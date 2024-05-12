59d97a8ef89c642043ee340ae428406bc02eee94
Naimur00 <aminaim4u@gmail.com>
2024-05-12 12:46:27 +0600

    Delete confirmation modal added

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -6,7 +6,6 @@ import toast from "react-hot-toast";
 import React from "react";
 import Modal from "react-modal";
 import DatePicker from "react-datepicker";
-import { BiBorderRadius } from "react-icons/bi";
 
 const MyPostedJobs = () => {
   const { user } = useContext(AuthContext);
@@ -45,7 +44,7 @@ const MyPostedJobs = () => {
     }
   };
 
-  // Modal
+  // update Modal
   const customStyles = {
     content: {
       top: "55%",
@@ -70,6 +69,9 @@ const MyPostedJobs = () => {
   function closeModal() {
     setModalIsOpen(false);
   }
+
+  // Delete confirmation modal
+  const [openDeleteModal, setOpenModal] = useState(false);
 
   return (
     <section className="container p-20 pt-24 mx-auto">
@@ -185,9 +187,7 @@ const MyPostedJobs = () => {
                           </button>
 
                           <button
-                            onClick={() => {
-                              handleDelete(job._id);
-                            }}
+                            onClick={() => setOpenModal(true)}
                             className="text-gray-500 transition-colors duration-200   hover:text-white focus:outline-none text-2xl"
                           >
                             <MdDelete />
@@ -345,6 +345,79 @@ const MyPostedJobs = () => {
           </div>
         </form>
       </Modal>
+      {/* Delete confirmation modal */}
+      {jobs.map((job) => (
+        <div
+          key={job._id}
+          onClick={() => setOpenModal(false)}
+          className={`fixed z-[100] flex items-center justify-center ${
+            openDeleteModal ? "opacity-1 visible" : "invisible opacity-0"
+          } inset-0 duration-100 bg-black/10`}
+        >
+          <div
+            onClick={(e_) => e_.stopPropagation()}
+            className={`absolute w-80 rounded-lg bg-white p-6 text-center drop-shadow-2xl dark:bg-gray-800 dark:text-white ${
+              openDeleteModal
+                ? "opacity-1 translate-y-0 duration-100"
+                : "translate-y-20 opacity-0 duration-100"
+            }`}
+          >
+            <div className="flex flex-col items-center justify-center space-y-4">
+              <svg
+                className="w-16 stroke-white/20"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <g strokeWidth="0"></g>
+                <g strokeLinecap="round" strokeLinejoin="round"></g>
+                <g>
+                  <path
+                    opacity="0.4"
+                    d="M12 7.75V13"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    d="M21.0802 8.58003V15.42C21.0802 16.54 20.4802 17.58 19.5102 18.15L13.5702 21.58C12.6002 22.14 11.4002 22.14 10.4202 21.58L4.48016 18.15C3.51016 17.59 2.91016 16.55 2.91016 15.42V8.58003C2.91016 7.46003 3.51016 6.41999 4.48016 5.84999L10.4202 2.42C11.3902 1.86 12.5902 1.86 13.5702 2.42L19.5102 5.84999C20.4802 6.41999 21.0802 7.45003 21.0802 8.58003Z"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                  <path
+                    opacity="0.4"
+                    d="M12 16.2002V16.3002"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  ></path>
+                </g>
+              </svg>
+              <h6 className="text-center text-sm font-medium opacity-70">
+                Are you sure, want to delete this item?
+              </h6>
+              <div className="flex gap-2">
+                <button
+                  onClick={() => {
+                    handleDelete(job._id);
+                    setOpenModal(false);
+                  }}
+                  className="rounded-md bg-black/40 px-6 py-2 text-sm text-white hover:bg-black/80"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => setOpenModal(false)}
+                  className="rounded-md border border-black px-6 py-2 text-sm text-white hover:bg-black/80"
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </section>
   );
 };
