29de4fc4fd7fbbcd497a936c88b942b4f8fba9b0
Naimur00 <aminaim4u@gmail.com>
2024-05-15 21:12:07 +0600

    skeleton added on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,24 @@ const Navbar = () => {
     }
   };
 
+  if (!user) {
+    return (
+      <section className="bg-transparent dark:bg-transparent container px-20 fixed top-0 left-0 right-0">
+        <div className="container flex flex-col items-center justify-between p-6 mx-auto space-y-4 animate-pulse sm:space-y-0 sm:flex-row">
+          <p className="w-32 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+          <div className="flex flex-wrap items-center justify-center gap-6 mt-10">
+            <p className="w-20 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p className="w-20 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p className="w-20 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p className="w-20 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p className="w-20 h-2 bg-gray-200 rounded-lg dark:bg-gray-700"></p>
+            <p className="w-8 h-8 bg-gray-200 rounded-full dark:bg-gray-700"></p>
+          </div>
+        </div>
+      </section>
+    );
+  }
+
   return (
     <div className="top-0 left-0 right-0 fixed z-10 ">
       <div className="navbar backdrop-blur-sm shadow-sm container px-4 lg:px-20 mx-auto text-white">
@@ -179,7 +197,10 @@ const Navbar = () => {
           <ul className="menu menu-horizontal px-1">
             {!user && (
               <li>
-                <Link to="/login" className="btn bg-none dark:bg-white/60 text-black/90">
+                <Link
+                  to="/login"
+                  className="btn bg-none dark:bg-white/60 text-black/90"
+                >
                   Login
                 </Link>
               </li>
